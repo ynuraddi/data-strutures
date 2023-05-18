8c72ab0c6d2f812288fa416b7c47d8bfe6ae107f
ynuraddi <definston@gmail.com>
2023-05-18 22:07:07 +0600

    binary

--- binrary-search/4-median-sorted-arrays/main.go
+++ binrary-search/4-median-sorted-arrays/main.go
@@ -1,25 +1,52 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println(findMedianSortedArrays([]int{1, 2, 3, 4, 5, 6, 7, 8}, []int{1, 2, 3, 4, 5}))
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	var num1, num2 int
-
-	for i, j, count := 0, 0, 0; count < (len(nums1)+len(nums2))/2; {
-		if i < len(nums1) && (j == len(nums2) || nums1[i] <= nums2[j]) {
-			num1, num2 = num2, nums1[i]
-			i++
-			count++
-		}
-
-		if j < len(nums2) && (i == len(nums1) || nums2[j] <= nums1[i]) {
-			num1, num2 = num2, nums2[j]
-			j++
-			count++
-		}
-
+	if l := len(nums1) + len(nums2); l%2 == 0 {
+		return (findKth(nums1, nums2, l/2-1) + findKth(nums1, nums2, l/2)) / 2.0
+	} else {
+		return findKth(nums1, nums2, l/2)
 	}
+}
 
-	return float64(num1+num2) / 2
+func findKth(nums1 []int, nums2 []int, k int) float64 {
+	for {
+		l1, l2 := len(nums1), len(nums2)
+		m1, m2 := l1/2, l2/2
+
+		if l1 == 0 {
+			return float64(nums2[k])
+		} else if l2 == 0 {
+			return float64(nums1[k])
+		} else if k == 0 {
+			if n1, n2 := nums1[0], nums2[0]; n1 <= n2 {
+				return float64(n1)
+			} else {
+				return float64(n2)
+			}
+		}
+
+		if k <= m1+m2 {
+			if nums1[m1] <= nums2[m2] {
+				nums2 = nums2[:m2]
+			} else {
+				nums1 = nums1[:m1]
+			}
+		} else {
+			if nums1[m1] <= nums2[m2] {
+				nums1 = nums1[m1+1:]
+				k -= m1 + 1
+			} else {
+				nums2 = nums2[m2+1:]
+				k -= m2 + 1
+			}
+		}
+	}
 }
